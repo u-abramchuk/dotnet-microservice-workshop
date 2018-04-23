03d0b2f99be8923f7abdbfdc678c508e14f5b5f2
Uladzimir Abramchuk <abramchuk@targetprocess.com>
2018-04-23 10:10:05 +0300

    add health check

--- src/Workshop/Program.cs
+++ src/Workshop/Program.cs
@@ -19,6 +19,7 @@ namespace Workshop
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseHealthChecks("/health")
                 .UseStartup<Startup>();
     }
 }
--- src/Workshop/Startup.cs
+++ src/Workshop/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.HealthChecks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading;
@@ -26,6 +27,10 @@ namespace Workshop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks(checks =>
+            {
+                checks.AddCheck("graceful-shutdown", () => SigtermReceived ? HealthCheckResult.Unhealthy("Shutting down") : HealthCheckResult.Healthy("OK"), TimeSpan.FromMilliseconds(100));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
