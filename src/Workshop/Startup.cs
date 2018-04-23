cf2391684f3cfb3263fbed41a9b0a87b1918588e
Uladzimir Abramchuk <abramchuk@targetprocess.com>
2018-04-23 10:06:36 +0300

    add graceful shutdown

--- src/Workshop/Startup.cs
+++ src/Workshop/Startup.cs
@@ -6,11 +6,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
 
 namespace Workshop
 {
     public class Startup
     {
+        private static bool SigtermReceived { get; set; }
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -18,17 +29,26 @@ namespace Workshop
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
         {
+            appLifetime.ApplicationStopping.Register(() =>
+            {
+                SigtermReceived = true;
+
+                var timeout = Configuration.GetValue<int?>("GRACEFUL_SHUTDOWN_TIMEOUT") ?? 2;
+
+                Thread.Sleep(timeout * 1000);
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                    {
+                        await context.Response.WriteAsync("Hello World!");
+                    });
         }
     }
 }
